319a6ada1cdc4e0aecafe04471156f380a2422e5
Conrad Davis Jr <conrad.davis@akqa.com>
2016-05-26 23:47:35 -0700

    styles update animation polish, layouts addition, and variables implementation

--- websites/BPN/www/site/templates/home.php
+++ websites/BPN/www/site/templates/home.php
@@ -67,13 +67,16 @@
 				</div> <!-- page wrapper -->
 			</div> <!-- container -->
 			<nav id="nav-menu" class="outer-nav right vertical">
-				<a href="#" class="icon-home">Home</a>
-				<a href="#" class="icon-news">News</a>
-				<a href="#" class="icon-image">Images</a>
-				<a href="#" class="icon-upload">Uploads</a>
-				<a href="#" class="icon-star">Favorites</a>
-				<a href="#" class="icon-mail">Messages</a>
-				<a href="#" class="icon-lock">Security</a>
+				<div class="bounding-container">
+					<div class="nav-menu__logo"><img src="<?php echo $config->urls->assets?>/images/BPN-Logo.png" alt="BPN-logo"/></div>
+					<a href="#" class="icon-home">Home</a>
+					<a href="#" class="icon-news">News</a>
+					<a href="#" class="icon-image">Images</a>
+					<a href="#" class="icon-upload">Uploads</a>
+					<a href="#" class="icon-star">Favorites</a>
+					<a href="#" class="icon-mail">Messages</a>
+					<a href="#" class="icon-lock">Security</a>
+				</div>
 			</nav>
 		</div> <!-- perspective -->
 	</body>
