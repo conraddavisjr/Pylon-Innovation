a8c1fc32c96b6e3882390fbaa0f6eb568c192a87
Conrad Davis Jr <conrad.davis@akqa.com>
2016-06-04 20:45:51 -0700

    set php to JSON and cleaned up some stray code

--- websites/BPN/www/site/templates/events.php
+++ websites/BPN/www/site/templates/events.php
@@ -16,7 +16,22 @@
   <link rel="stylesheet" type="text/css" href="<?php echo $config->urls->site?>compiled-dev/css/events.css" />
 
   <script type="text/javascript">
-    var eventData = [{thumbnail:'/lab/Pylon-Innovation/websites/BPN/www/site/assets//images/16.jpg', title:'Test Post', subtitle:'subtitle', details:'details', photos:'header01.jpg|header02.jpg|header03.jpgitem06.jpg'}, {thumbnail:'/lab/Pylon-Innovation/websites/BPN/www/site/assets//images/16.jpg', title:'Test Post22', subtitle:'subtitle', details:'details', photos:'header01.jpg|header02.jpg|header03.jpgitem06.jpg'}];
+    var eventData = [ 
+      <?php $posts = $page->children();
+          // convert the posted event data to a JSON format 
+          foreach($posts as $post) { 
+
+            echo "{", 
+                   "thumbnail:'{$post->thumbnail->url}',",
+                   "title:'{$post->title}',",
+                   "subtitle:'{$post->subtitle}',",
+                   "details:'{$post->details}',",
+                   "photos:'{$post->photos->url}'",
+                 "},";
+          }
+      ?>
+    ]   
+
   </script>
 </head>
 
@@ -38,32 +53,6 @@
       </div>
     </nav>
     <div class="content">
-      <p class="info">Please choose a category</p>
-      <figure class="teaser" content-id="1">
-        <img src="<?php echo $config->urls->assets?>/images/16.jpg" alt="img16"/>
-        <figcaption>
-          <h2>Noisy <span>Terry</span></h2>
-          <p>
-            <a href="#"><i class="fa fa-fw fa-download"></i></a>
-            <a href="#"><i class="fa fa-fw fa-heart"></i></a>
-            <a href="#"><i class="fa fa-fw fa-share"></i></a>
-            <a href="#"><i class="fa fa-fw fa-tags"></i></a>
-          </p>
-        </figcaption>     
-      </figure>
-      <figure class="teaser">
-        <img src="<?php echo $config->urls->assets?>/images/16.jpg" alt="img16"/>
-        <figcaption>
-          <h2>Noisy <span>Terry</span></h2>
-          <p>
-            <a href="#"><i class="fa fa-fw fa-download"></i></a>
-            <a href="#"><i class="fa fa-fw fa-heart"></i></a>
-            <a href="#"><i class="fa fa-fw fa-share"></i></a>
-            <a href="#"><i class="fa fa-fw fa-tags"></i></a>
-          </p>
-        </figcaption>     
-      </figure>
-
       <article>
         <img src="<?php echo $config->urls->assets?>/images/16.jpg">
         <div class="close-content-btn"></div>
@@ -81,90 +70,34 @@
   <script type="text/javascript" src="<?php echo $config->urls->site?>compiled-dev/js/lib/modernizr.js"></script>
   <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.4/TweenMax.min.js"></script>
-
-  <script src="<?php echo $config->urls->site?>compiled-dev/js/lib/classie.js"></script>
-  <script src="<?php echo $config->urls->site?>compiled-dev/js/dummydata.js"></script>
-  <script src="<?php echo $config->urls->site?>compiled-dev/js/filter-nav.min.js"></script>
   <script src="<?php echo $config->urls->site?>compiled-dev/js/teaser-zoom.js"></script>
-
-
-  <script type="text/javascript">
-
-
-
-
-    $('body').data("<?php $posts = $page->children();
-       
-        foreach($posts as $post) {      
-
-
-          echo "{$post->thumbnail}",
-               "{$post->title}",
-               "{$post->subtitle}",
-               "{$post->details}",
-               "{$post->photos}";
-        }
-        
-    ?>");    
-    
-  </script>
-
 
 
   <script>
     (function() {
-      var menuEl = document.getElementById('ml-menu'),
-        mlmenu = new MLMenu(menuEl, {
-          // breadcrumbsCtrl : true, // show breadcrumbs
-          // initialBreadcrumb : 'all', // initial breadcrumb text
-          backCtrl : false, // show back button
-          // itemsDelayInterval : 60, // delay between each menu item sliding animation
-          onItemClick: loadDummyData // callback: item that doesn´t have a submenu gets clicked - onItemClick([event], [inner HTML of the clicked item])
-        });
 
-      // mobile menu toggle
-      var openMenuCtrl = document.querySelector('.action--open'),
-        closeMenuCtrl = document.querySelector('.action--close');
+      // load the content
 
-      openMenuCtrl.addEventListener('click', openMenu);
-      closeMenuCtrl.addEventListener('click', closeMenu);
+      loadEventData();
 
-      function openMenu() {
-        classie.add(menuEl, 'menu--open');
-      }
+      function loadEventData() {
 
-      function closeMenu() {
-        classie.remove(menuEl, 'menu--open');
-      }
-
-      // simulate grid content loading
-      var gridWrapper = document.querySelector('.content');
-
-      function loadDummyData(ev, itemName) {
-        ev.preventDefault();
-
-        closeMenu();
-        gridWrapper.innerHTML = '';
-        classie.add(gridWrapper, 'content--loading');
-        setTimeout(function() {
-          classie.remove(gridWrapper, 'content--loading');
-          for (var i = 0; i < eventData.length; i++) {
-            console.log('i: ', i);
-            $('.content').append(
-            '<figure class="teaser" content-id="1">' +
-              '<img src="' + eventData[i].thumbnail + '" alt="img16"/>' +
-              '<figcaption>' +
-                '<h2>' + eventData[i].title + '</h2>' + 
-                '<p>' + 
-                  '<a href="#"><i class="fa fa-fw fa-download"></i></a>' + 
-                  '<a href="#"><i class="fa fa-fw fa-heart"></i></a>' +
-                  '<a href="#"><i class="fa fa-fw fa-share"></i></a>' +
-                  '<a href="#"><i class="fa fa-fw fa-tags"></i></a>' +
-                '</p>' +
-              '</figcaption>' +
-            '</figure>');
-          }
-        }, 700);
+        for (var i = 0; i < eventData.length; i++) {
+          console.log('i: ', i);
+          $('.content').append(
+          '<figure class="teaser" content-id="1">' +
+            '<img src="' + eventData[i].thumbnail + '" alt="img16"/>' +
+            '<figcaption>' +
+              '<h2>' + eventData[i].title + '</h2>' + 
+              '<p>' + 
+                '<a href="#"><i class="fa fa-fw fa-download"></i></a>' + 
+                '<a href="#"><i class="fa fa-fw fa-heart"></i></a>' +
+                '<a href="#"><i class="fa fa-fw fa-share"></i></a>' +
+                '<a href="#"><i class="fa fa-fw fa-tags"></i></a>' +
+              '</p>' +
+            '</figcaption>' +
+          '</figure>');
+        }
       }
     })();
   </script>
