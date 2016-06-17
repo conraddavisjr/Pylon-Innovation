c1f0ca60deca0ae8f2979d7e350b756f193a0680
Conrad Davis Jr <conrad.davis@akqa.com>
2016-06-16 18:02:38 -0700

    added the home link to the navigation

--- websites/BPN/www/site/templates/includes/page-wrapper-bottom.php
+++ websites/BPN/www/site/templates/includes/page-wrapper-bottom.php
@@ -5,6 +5,9 @@
       <!-- Menu Items -->
       <?php
         $root = $pages->get("/");
+        // first output the home link
+        echo "<a href='{$root->url}' class=\"icon-home navigation-link\">{$root->title}</a>";
+        // then output the other links
         $children = $root->children();
         foreach($children as $child) {
           echo "<a href='{$child->url}' class=\"icon-home navigation-link\">{$child->title}</a>";
