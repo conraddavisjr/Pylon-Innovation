d1a15538018d23efbd39930dfc8a5245c9324718
Conrad Davis Jr <conrad.davis@akqa.com>
2016-07-04 22:22:21 -0700

    mpbile nav work

--- websites/BPN/www/site/templates/includes/page-wrapper-bottom.php
+++ websites/BPN/www/site/templates/includes/page-wrapper-bottom.php
@@ -5,12 +5,18 @@
       <!-- Menu Items -->
       <?php
         $root = $pages->get("/");
+        $menuIcons = array(
+          "Events" => "fa-users",
+          "News" => "fa-newspaper-o",
+          "Contact" => "fa-phone-square"
+        );
+
         // first output the home link
         echo "<a href='{$root->url}' class=\"icon-home navigation-link\">{$root->title}</a>";
         // then output the other links
         $children = $root->children();
         foreach($children as $child) {
-          echo "<a href='{$child->url}' class=\"icon-home navigation-link\">{$child->title}</a>";
+          echo "<a href='{$child->url}' class=\"navigation-link\"><i class=\"fa {$menuIcons[$child->title]}\"></i>{$child->title}</a>";
         }
       ?>
     </div>
